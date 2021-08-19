ec76388ddc5f5bfd33e5c921b920657e40f32f7b
Fatimah Alhelal <31361308+fatmahhelal@users.noreply.github.com>
2021-08-19 13:32:05 +0300

    Updates

---
+++ precache-manifest.02d9a12e3e74c014375002ea465d81a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d202236104708d5384fe",
+    "url": "/myreads-react/static/css/main.6e02ce59.chunk.css"
+  },
+  {
+    "revision": "d202236104708d5384fe",
+    "url": "/myreads-react/static/js/main.d2022361.chunk.js"
+  },
+  {
+    "revision": "0f2b5974e8414f4578c2",
+    "url": "/myreads-react/static/js/1.0f2b5974.chunk.js"
+  },
+  {
+    "revision": "3923c7c42fa552ff5f3a",
+    "url": "/myreads-react/static/js/runtime~main.3923c7c4.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-react/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-react/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-react/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "f4ec380e53ddf06460e628689415d940",
+    "url": "/myreads-react/index.html"
+  }
+];
